6194136a9a64bb35cc936769338741dd7932b46b
Greyson Dobbels <greysonfox6464@gmail.com>
2026-06-28 17:28:16 -0500

    Created the spring boot porject with a basic get endpoint to test if it works. I used jdk 25 so you will need to install that if you don't have it on your machine to work on the project. I would recommend looking at the docs to see what spring boot is but it isn;t that compliacated if you know how the web works. I will add more routes later

---
+++ Summarizer/src/main/java/com/c4g_team_7/Summarizer/SummarizerApplication.java
@@ -0,0 +1,20 @@
+package com.c4g_team_7.Summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SummarizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummarizerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    String home() {
+        return "Hi";
+    }
+}
